e65619f773c04b6c0ec73a7e53c20b87cefb0936
banjuanshu <bjs.null@gmail.com>
2019-04-26 10:16:22 +0800

    Added php dir and generateRandomString.php

---
+++ php/generateRandomString.php
@@ -0,0 +1,17 @@
+<?php
+
+
+function getRandomString($len, $chars=null)
+{
+    if (is_null($chars)) {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    }
+    mt_srand(10000000*(double)microtime());
+    for ($i = 0, $str = '', $lc = strlen($chars)-1; $i < $len; $i++) {
+        $str .= $chars[mt_rand(0, $lc)];
+    }
+    return $str;
+}
+
+
+var_dump(getRandomString(32));
